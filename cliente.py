968610c1b6fece1d31450ea21a3cacbe2b187f3c
Luísa Ribeiro Bezerra <luisarbezerra@hotmail.com>
2018-09-22 00:10:42 -0300

    Done.

--- cliente.py
+++ cliente.py
@@ -1,23 +1,29 @@
 import sys
 import socket
+import struct
+import time
 
-HOST     = sys.argv[1]
-PORT     = int(sys.argv[2])
-palavra  = sys.argv[3]
-chave    = sys.argv[4]
-tcp      = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-destino  = (HOST, PORT)
+HOST    = sys.argv[1]
+PORT    = int(sys.argv[2])
+palavra = sys.argv[3]
+tamanho = len(palavra)
+rotacao = int(sys.argv[4])
+tcp     = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+destino = (HOST, PORT)
 
-alfabeto = 'abcdefghijklmnopqrstuvwxyz'
+mensagem_codificada = []
 
+for c in palavra:
+    rotacao %= 25
+    tmp = (ord(c) - 97 + rotacao) % 26 + 97
+    mensagem_codificada += chr(tmp if 97 <= tmp <= 122 else 96 + tmp % 122)
+
+mensagem = ''.join(mensagem_codificada)
 tcp.connect(destino)
-print('Para sair use CTRL+X\n')
-msg = input()
+tcp.send(struct.pack('!i', tamanho))
+tcp.send(mensagem.encode('ASCII', 'ignore'))
+tcp.send(struct.pack('!i', rotacao))
 
-while msg != '\x18':
-    tcp.send(msg.encode('latin1'))
-    msg = tcp.recv(1024).decode('latin1')
-    print(msg)
-    msg = input()
-
+palavra_decodificada = tcp.recv(tamanho)
+print(palavra_decodificada.decode())
 tcp.close()
--- servidor.py
+++ servidor.py
@@ -1,23 +1,59 @@
 import socket
+import sys
+import struct
+import threading
 
-HOST   = '' # Endereco IP do Servidor
-PORT   = 5000 # Porta que o Servidor esta
-tcp    = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-origem = (HOST, PORT)
+class ServidorThread(object):
+    def __init__(self, HOST, PORT):
+        self.host = HOST
+        self.port = PORT
+        self.tcp  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.tcp.bind((self.host, self.port))
 
-tcp.bind(origem)
-tcp.listen(1)
-print('\nServidor TCP iniciado no IP', HOST, 'na porta', PORT)
-while True:
-    conexao, cliente = tcp.accept()
-    print('Conectado por', cliente)
+    def listen(self):
+        self.tcp.listen(5)
+        while True:
+            client, address = self.tcp.accept()
+            client.settimeout(15)
+            threading.Thread(target = self.cifraDeCesar,args = (client,address)).start()
 
+    def cifraDeCesar(self, client, address):
+        while True:
+            try:
+                tamanho = client.recv(4)
+                tamanho = struct.unpack('!i', tamanho)[0]
+                palavra = client.recv(tamanho)
+                palavra = palavra.decode()
+                rotacao = client.recv(4)
+                rotacao = struct.unpack('!i', rotacao)[0]
+
+                mensagem_decodificada = []
+
+                for c in palavra:
+                    rotacao %= 26
+                    tmp = (ord(c) - 97 - rotacao) % 26 + 97
+                    mensagem_decodificada += chr(tmp if 97 <= tmp <= 122 else 96 + tmp % 122)
+                
+                mensagem_final = ''.join(mensagem_decodificada)
+                if palavra:
+                    # Set the response to echo back the recieved data
+                    print(mensagem_final)
+                    response = mensagem_final
+                    client.send(response.encode('ASCII', 'ignore'))
+                else:
+                    raise error('Client disconnected')
+            except:
+                client.close()
+                return False
+
+if __name__ == '__main__':
     while True:
-        mensagem = conexao.recv(1024).decode('latin1')
-        if not mensagem: 
+        port_num = sys.argv[1]
+        try:
+            port_num = int(port_num)
             break
-        msg = '[' + mensagem + ']'
-        print(cliente, msg)
-        conexao.send(msg.encode('latin1'))
-    print('Finalizando conexao do cliente', cliente)
-    conexao.close()
+        except ValueError:
+            pass
+
+    ServidorThread('127.0.0.1',port_num).listen()
